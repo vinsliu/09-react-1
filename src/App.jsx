a04914d466a5497bf52992c47f87f35b1a4a0d81
Vincent <vincent00lav@outlook.com>
2025-05-12 13:48:43 +0200

    Activity 4 finished

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import "./App.css";
 import Layout from "./components/Layout";
 import Category from "./pages/Category";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
         <Route path="/category/:categoryId" element={<Category />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   );
