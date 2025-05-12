31387f411f968763974c4a1d97277a5f072f200d
Vincent <vincent00lav@outlook.com>
2025-05-12 15:17:38 +0200

    Activity 5 finished 1.2

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Container } from "react-bootstrap";
 
 function Layout() {
   return (
     <>
       <Header />
       <main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,5 @@
-import { Container } from "react-bootstrap";
-
 function Home() {
-  return (
-    <main>
-      <Container>
-        <h1>Welcome Home !</h1>
-      </Container>
-    </main>
-  );
+  return <h1>Welcome Home !</h1>;
 }
 
 export default Home;
