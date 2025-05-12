665eef28feecdebf227c5088b02bdfa4ff2236fa
Vincent <vincent00lav@outlook.com>
2025-05-12 16:43:32 +0200

    Activity 8 finished

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,10 +1,17 @@
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 
 const Category = () => {
   const { categoryId } = useParams();
+  const navigate = useNavigate();
+  const handleReturnToHome = () => {
+    navigate("/");
+  };
 
   return (
     <div>
+      <button onClick={handleReturnToHome} className="btn btn-dark">
+        Retour à la page d'accueil
+      </button>
       <h1>{categoryId}</h1>
     </div>
   );
